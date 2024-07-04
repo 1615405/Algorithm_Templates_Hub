740fb77a77c00a116f3ddce89ce11f912b2d745a
cherry_77 <1615405@qq.com>
2024-07-04 14:51:37 +0800

    modify files 2024-07-04

--- 数据结构/linked_list.py
+++ 数据结构/linked_list.py
@@ -21,19 +21,19 @@ def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[Li
 
 
 def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = cur = ListNode()        
-        while list1 and list2:
-            if list1.val <= list2.val:
-                cur.next = list1
-                list1 = list1.next
-            else:
-                cur.next = list2
-                list2 = list2.next
-            cur = cur.next
-        
-        cur.next = list1 if list1 else list2
+    dummy = cur = ListNode()        
+    while list1 and list2:
+        if list1.val <= list2.val:
+            cur.next = list1
+            list1 = list1.next
+        else:
+            cur.next = list2
+            list2 = list2.next
+        cur = cur.next
+    
+    cur.next = list1 if list1 else list2
 
-        return dummy.next
+    return dummy.next
 
 
 
