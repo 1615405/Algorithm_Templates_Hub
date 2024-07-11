07cf6a5894c80409dabb12eaeec5f52b39f4cce7
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-11 19:47:23 +0800

    Update Template_StringManipulation.py

--- Template_StringManipulation.py
+++ Template_StringManipulation.py
@@ -15,3 +15,37 @@ def longestCommonPrefix(strs: List[str]) -> str:
         lcp += 1
     
     return strs[0][:lcp]
+
+
+def strStr(haystack: str, needle: str) -> int:
+    """
+    在字符串 haystack 中查找子字符串 needle 的第一个出现的位置。如果 needle 不是 haystack 的一部分，则返回 -1。
+
+    参数：
+        haystack (str): 待搜索的主字符串。
+        needle (str): 需要搜索的子字符串。
+
+    返回：
+        int: 子字符串 needle 第一次出现的位置，如果未找到则返回 -1。
+    
+    方法：
+        1. 如果 needle 的长度大于 haystack，直接返回 -1。
+        2. 计算 needle 的哈希值。
+        3. 通过滑动窗口计算 haystack 中每个子串的哈希值，比较哈希值以确定是否匹配。
+    """
+    if len(needle) > len(haystack):
+        return -1
+    
+    def hash_function(s):
+        import hashlib
+        return hashlib.md5(s.encode()).hexdigest()
+    
+    needle_hash = hash_function(needle)
+    
+    for i in range(len(haystack) - len(needle) + 1):
+        current_window = haystack[i:i+len(needle)]
+        window_hash = hash_function(current_window)
+        if window_hash == needle_hash:
+            return i
+    
+    return -1
