351239bc0ea351e088052b717f7c9c8ede0557a3
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-12 17:25:32 +0800

    Update Template_StackQueueOperation.py

--- Template_StackQueueOperation.py
+++ Template_StackQueueOperation.py
@@ -24,3 +24,53 @@ def isValid(s: str) -> bool:
             stack.pop()  # 匹配成功，弹出栈顶括号
 
     return len(stack) == 0  # 栈为空表示所有括号都匹配成功
+
+
+class MyStack:
+    """
+    使用单个队列实现栈的数据结构。栈是一种后进先出（LIFO）的数据结构，只允许在栈顶进行添加和删除操作。
+    """
+    def __init__(self):
+        from collections import deque
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        n = len(self.queue)
+        self.queue.append(x)
+        for _ in range(n):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return not self.queue
+
+
+class MyQueue:
+    """
+    使用两个栈实现一个队列。队列是一种先进先出（FIFO）的数据结构，元素从队列的一端添加，从另一端移除。
+    """
+    def __init__(self):
+        self.A, self.B = [], []
+
+    def push(self, x: int) -> None:
+        self.A.append(x)
+
+    def pop(self) -> int:
+        peek = self.peek()
+        self.B.pop()
+        return peek
+
+    def peek(self) -> int:
+        if self.B:  return self.B[-1]
+        if not self.A: return -1
+        while self.A:
+            self.B.append(self.A.pop())
+        return self.B[-1]
+
+    def empty(self) -> bool:
+        return not self.A and not self.B
