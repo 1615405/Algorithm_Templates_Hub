157548ea88fbe8a28ae984ce1000e066312bd75c
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-16 12:25:37 +0800

    Update Template_02_BinaryTree.py

--- Tree-Based Algorithms Catalog/Template_02_BinaryTree.py
+++ Tree-Based Algorithms Catalog/Template_02_BinaryTree.py
@@ -26,22 +26,6 @@ def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
 
 
-def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
-    """
-    翻转一棵二叉树。每个节点的左右子树会被交换。
-
-    参数:
-        root (Optional[TreeNode]): 二叉树的根节点。
-
-    返回:
-        Optional[TreeNode]: 翻转后的二叉树的根节点。
-    """
-    if not root:
-        return None
-    root.left, root.right = invertTree(root.right), invertTree(root.left)
-    return root
-
-
 def isBalanced(root: Optional[TreeNode]) -> bool:
     """
     判断一棵二叉树是否是高度平衡的。高度平衡的二叉树是指任何一个节点的两个子树的高度差不超过1。
@@ -62,62 +46,6 @@ def isBalanced(root: Optional[TreeNode]) -> bool:
     if abs(height(root.left) - height(root.right)) > 1:
         return False
     return isBalanced(root.left) and isBalanced(root.right)
-
-
-def isSubtree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-    """
-    判断一个二叉树是否是另一个二叉树的子树。
-    
-    参数:
-        root (TreeNode, 可选): 主树的根节点。
-        subRoot (TreeNode, 可选): 子树的根节点。
-
-    返回:
-        bool: 如果 subRoot 是 root 的子树，返回 True，否则返回 False。
-    """
-    def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p or not q:  return p is q
-        if p.val != q.val:  return False
-        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
-    
-    if not root or not subRoot:
-        return root is subRoot
-    return isSameTree(root, subRoot) or isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot)
-
-
-def mergeTrees(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-    """
-    合并两棵二叉树，每个对应节点的值相加，如果一个位置只有一个树有节点，则直接使用该节点。
-
-    参数:
-        root1 (TreeNode, 可选): 第一棵二叉树的根节点。
-        root2 (TreeNode, 可选): 第二棵二叉树的根节点。
-
-    返回:
-        TreeNode, 可选: 合并后的二叉树的根节点，如果两棵树均为空，则返回 None。
-    """
-    if not root1:  return root2
-    if not root2:  return root1
-    
-    return TreeNode(root1.val + root2.val,
-        mergeTrees(root1.left, root2.left),
-        mergeTrees(root1.right, root2.right))
-
-
-def maxDepth(root: Optional[TreeNode]) -> int:
-    """
-    计算二叉树的最大深度。最大深度是从根节点到最远叶子节点的最长路径上的节点数。
-    
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-    
-    返回：
-        int: 二叉树的最大深度。
-    """
-    if root is None: return 0
-    l_depth = maxDepth(root.left)
-    r_depth = maxDepth(root.right)
-    return max(l_depth, r_depth) + 1
 
 
 def minDepth(root: Optional[TreeNode]) -> int:
