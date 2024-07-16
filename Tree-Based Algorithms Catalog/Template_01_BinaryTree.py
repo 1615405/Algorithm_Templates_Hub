83e5463f267149c22d388bd6a5eb7580a89b802f
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-16 14:29:05 +0800

    Update Template_01_BinaryTree.py

--- Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
+++ Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
@@ -89,12 +89,6 @@ def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 def isBalanced(root: Optional[TreeNode]) -> bool:
     """
     判断一棵二叉树是否是高度平衡的。高度平衡的二叉树是指任何一个节点的两个子树的高度差不超过1。
-    
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-    
-    返回：
-        bool: 如果二叉树是高度平衡的，则返回True；否则返回False。
     """
     def height(root: Optional[TreeNode]) -> int:
         if not root:  return 0
@@ -125,12 +119,6 @@ def sumOfLeftLeaves(root: Optional[TreeNode]) -> int:
 def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
     """
     计算二叉树的直径，即二叉树中任意两个节点之间的最长路径的边数。
-    
-    参数:
-        root (TreeNode, 可选): 二叉树的根节点。
-
-    返回:
-        int: 二叉树的直径。
     """
     ans = 0
     def dfs(node: Optional[TreeNode]) -> int:
@@ -148,12 +136,6 @@ def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
 def findTilt(root: Optional[TreeNode]) -> int:
     """
     计算二叉树的所有节点的倾斜度总和。节点的倾斜度定义为该节点的左子树节点值之和与右子树节点值之和的绝对差。
-    
-    参数:
-        root (TreeNode, 可选): 二叉树的根节点。
-    
-    返回:
-        int: 所有节点的倾斜度之和。
     """
     ans = 0
     def dfs(root: Optional[TreeNode]) -> int:
@@ -169,13 +151,7 @@ def findTilt(root: Optional[TreeNode]) -> int:
 
 def countNodes(root: Optional[TreeNode]) -> int:
     """
-    计算完全二叉树中的节点数。利用二分查找结合位操作，以优化性能，避免直接的完全遍历。
-
-    参数：
-        root (Optional[TreeNode]): 完全二叉树的根节点。
-
-    返回：
-        int: 树中的节点总数。
+    计算完全二叉树中的节点数。
     """
     def exists(root: Optional[TreeNode], level: int, k: int) -> bool:
         bits = 1 << (level - 1)
