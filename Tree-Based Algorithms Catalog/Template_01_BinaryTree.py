b3d0c9f92d6cd383e7f0f3afa03f947701505a7b
cherry_77 <1615405@qq.com>
2024-07-16 12:05:12 +0800

    Create files

--- Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
+++ Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
@@ -17,10 +17,10 @@ def preorderTraversal(root: Optional[TreeNode]) -> List[int]:
         List[int]: 存储先序遍历结果的列表。
     """
     if not root: return []
-    res, stack = [], [root] # 利用栈进行临时存储
+    res, stack = [], [root]
     while stack:
-        node = stack.pop() # 取出一个节点，表示开始访问以该节点为根的子树
-        res.append(node.val) # 首先访问该节点（先序），之后顺序入栈右子树，左子树
+        node = stack.pop()
+        res.append(node.val)
         if node.right:
             stack.append(node.right)
         if node.left:
@@ -41,14 +41,14 @@ def inorderTraversal(root: Optional[TreeNode]) -> List[int]:
     if not root: return []
     res, stack = [], []
     while root:
-        if root.left: # 存在左子树，入栈并深入左子树
+        if root.left:
             stack.append(root)
             root = root.left
-        else: # 否则就可以寻找深入右子树的节点
-            while stack and not root.right: # 对于不能深入右子树的节点，直接访问，此时子树访问结束
+        else:
+            while stack and not root.right:
                 res.append(root.val)
                 root = stack.pop()
-            res.append(root.val) # 如果可以深入右子树，访问该节点后，深入右子树
+            res.append(root.val)
             root = root.right
     return res
 
@@ -65,13 +65,12 @@ def postorderTraversal(root: Optional[TreeNode]) -> List[int]:
     """
     if not root: return []
     res, stack = [], [root]
-    prev = root # 为了判断父子节点关系
+    prev = root
     while stack:
-        root = stack.pop() # 取出一个节点，表示开始访问以该节点为根的子树
-        if (not root.left and not root.right) or (root.left == prev or root.right == prev): # 如果该节点为叶子节点，或者已经访问该节点的子节点
-            res.append(root.val) # 直接访问
+        root = stack.pop()
+        if (not root.left and not root.right) or (root.left == prev or root.right == prev):
             prev = root
-        else: # 否则就顺序把当前节点，右孩子，左孩子入栈
+        else:
             stack.append(root)
             if root.right:
                 stack.append(root.right)
@@ -103,59 +102,6 @@ def binaryTreePaths(root: Optional[TreeNode]) -> List[str]:
     paths = []
     construct_paths(root, '')
     return paths
-
-
-def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    """
-    判断两棵二叉树是否相同。如果两棵树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
-
-    参数：
-        p (Optional[TreeNode]): 第一棵树的根节点。
-        q (Optional[TreeNode]): 第二棵树的根节点。
-    
-    返回：
-        bool: 如果两棵树相同，则返回 True，否则返回 False。
-    """
-    if not p or not q:
-        return p is q
-
-    if p.val != q.val:
-        return False
-        
-    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
-
-
-def isSymmetric(root: Optional[TreeNode]) -> bool:
-    """
-    检查给定的二叉树是否是对称的。
-
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-    
-    返回：
-        bool: 如果树是对称的，则返回 True，否则返回 False。
-    """
-    def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None or q is None:
-            return p is q
-        return p.val == q.val and isSameTree(p.left, q.right) and isSameTree(p.right, q.left)
-    
-    return isSameTree(root, root)
-
-
-def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
-    """
-    翻转一棵二叉树。每个节点的左右子树会被交换。
-
-    参数:
-        root (Optional[TreeNode]): 二叉树的根节点。
-
-    返回:
-        Optional[TreeNode]: 翻转后的二叉树的根节点。
-    """
-    if not root:  return None
-    root.left, root.right = invertTree(root.right), invertTree(root.left)
-    return root
 
 
 def maxDepth(root: Optional[TreeNode]) -> int:
@@ -219,30 +165,6 @@ def sortedArrayToBST(nums: List[int]) -> Optional[TreeNode]:
     return helper(0, len(nums) - 1)
 
 
-def isBalanced(root: Optional[TreeNode]) -> bool:
-    """
-    判断一棵二叉树是否是高度平衡的。高度平衡的二叉树是指任何一个节点的两个子树的高度差不超过1。
-    
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-    
-    返回：
-        bool: 如果二叉树是高度平衡的，则返回True；否则返回False。
-    """
-    def height(root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return max(height(root.left), height(root.right)) + 1
-    
-    if not root:
-        return True
-
-    if abs(height(root.left) - height(root.right)) > 1:
-        return False
-        
-    return isBalanced(root.left) and isBalanced(root.right)
-
-
 def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
     """
     判断二叉树中是否存在一条路径，该路径上的节点值之和等于给定的目标值 targetSum。
@@ -261,45 +183,6 @@ def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
         return targetSum == root.val
     
     return hasPathSum(root.left, targetSum - root.val) or hasPathSum(root.right, targetSum - root.val)
-
-
-def isSubtree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-    """
-    判断一个二叉树是否是另一个二叉树的子树。
-    
-    参数:
-        root (TreeNode, 可选): 主树的根节点。
-        subRoot (TreeNode, 可选): 子树的根节点。
-
-    返回:
-        bool: 如果 subRoot 是 root 的子树，返回 True，否则返回 False。
-    """
-    def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p or not q:  return p is q
-        if p.val != q.val:  return False
-        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
-    
-    if not root or not subRoot:
-        return root is subRoot
-    return isSameTree(root, subRoot) or isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot)
-
-
-def mergeTrees(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-    """
-    合并两棵二叉树，每个对应节点的值相加，如果一个位置只有一个树有节点，则直接使用该节点。
-
-    参数:
-        root1 (TreeNode, 可选): 第一棵二叉树的根节点。
-        root2 (TreeNode, 可选): 第二棵二叉树的根节点。
-
-    返回:
-        TreeNode, 可选: 合并后的二叉树的根节点，如果两棵树均为空，则返回 None。
-    """
-    if not root1:  return root2
-    if not root2:  return root1
-    return TreeNode(root1.val + root2.val,
-        mergeTrees(root1.left, root2.left),
-        mergeTrees(root1.right, root2.right))
 
 
 def averageOfLevels(root: Optional[TreeNode]) -> List[float]:
--- Tree-Based Algorithms Catalog/Template_02_BinaryTree.py
+++ Tree-Based Algorithms Catalog/Template_02_BinaryTree.py
@@ -6,232 +6,105 @@
 #         self.right = right
 
 
-# Definition for a Node.
-# class Node:
-#     def __init__(self, val=None, children=None):
-#         self.val = val
-#         self.children = children
-
-
-def preorder(root: 'Node') -> List[int]:
+def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     """
-    对一个 N 叉树进行前序遍历，返回遍历的节点值列表。
-    
-    参数:
-        root (Node, 可选): N 叉树的根节点。
-    
-    返回:
-        List[int]: 前序遍历的节点值列表。
-    """
-    ans = []
-    def dfs(node: 'Node'):
-        if not root:  return
-        ans.append(node.val)
-        for child in node.children:
-            dfs(child)
-    dfs(root)
-    return ans
-
-
-def postorder(root: 'Node') -> List[int]:
-    """
-    对一个 N 叉树进行后序遍历，返回遍历的节点值列表。
-    
-    参数:
-        root (Node, 可选): N 叉树的根节点。
-    
-    返回:
-        List[int]: 后序遍历的节点值列表。
-    """
-    ans = []
-    def dfs(node: 'Node'):
-        if node is None:  return
-        for child in node.children:
-            dfs(child)
-        ans.append(node.val)
-    dfs(root)
-    return ans
-
-
-def maxDepth(root: 'Node') -> int:
-    """
-    计算 N 叉树的最大深度。
-    
-    参数:
-        root (Node, 可选): N 叉树的根节点。
-
-    返回:
-        int: 树的最大深度。
-    """
-    if not root:  return 0
-    ans = 0
-    for child in root.children:
-        ans = max(ans, maxDepth(child))
-    return ans + 1
-
-
-def countNodes(root: Optional[TreeNode]) -> int:
-    """
-    计算完全二叉树中的节点数。利用二分查找结合位操作，以优化性能，避免直接的完全遍历。
+    判断两棵二叉树是否相同。如果两棵树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
 
     参数：
-        root (Optional[TreeNode]): 完全二叉树的根节点。
-
+        p (Optional[TreeNode]): 第一棵树的根节点。
+        q (Optional[TreeNode]): 第二棵树的根节点。
+    
     返回：
-        int: 树中的节点总数。
+        bool: 如果两棵树相同，则返回 True，否则返回 False。
     """
-    def exists(root: Optional[TreeNode], level: int, k: int) -> bool:
-        bits = 1 << (level - 1)
-        while root and bits > 0:
-            if (bits & k) == 0:
-                root = root.left
-            else:
-                root = root.right
-            bits >>= 1
-        return root is not None
-    
-    if not root:  return 0
+    if not p or not q:
+        return p is q
 
-    level = 0
-    node = root
-    while node.left:
-        level += 1
-        node = node.left
-    
-    low, high = 1 << level, (1 << (level + 1)) - 1
-    while low < high:
-        mid = (low + high + 1) // 2
-        if exists(root, level, mid):
-            low = mid
-        else:
-            high = mid - 1
-    return low
+    if p.val != q.val:
+        return False
+        
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
 
 
-def sumOfLeftLeaves(root: Optional[TreeNode]) -> int:
+def invertTree(root: Optional[TreeNode]) -> Optional[TreeNode]:
     """
-    计算给定二叉树中所有左叶子节点的值之和。
+    翻转一棵二叉树。每个节点的左右子树会被交换。
 
     参数:
         root (Optional[TreeNode]): 二叉树的根节点。
 
     返回:
-        int: 所有左叶子节点的值之和。
-
-    描述:
-    函数通过递归深度优先搜索（DFS）遍历树。对于每个节点，检查其左子节点是否为叶子节点。如果是左叶子节点，则累加其值；如果不是，递归计算其左子节点。
-    对于右子节点，仅当它不是叶子节点时，才递归计算其右子节点的左叶子节点之和。
+        Optional[TreeNode]: 翻转后的二叉树的根节点。
     """
-    isLeafNode = lambda node: not node.left and not node.right
-    def dfs(node: Optional[TreeNode]) -> int:
-        ans = 0
-        if node.left:
-            ans += node.left.val if isLeafNode(node.left) else dfs(node.left)
-        if node.right and not isLeafNode(node.right):
-            ans += dfs(node.right)
-        return ans
-    return dfs(root) if root else 0
+    if not root:
+        return None
+    
+    root.left, root.right = invertTree(root.right), invertTree(root.left)
+    return root
 
 
-def findMode(root: Optional[TreeNode]) -> List[int]:
+def isBalanced(root: Optional[TreeNode]) -> bool:
     """
-    查找二叉搜索树中的众数(出现频率最高的元素)。
-
-    参数:
-        root (TreeNode, 可选): 二叉搜索树的根节点。
-
-    返回:
-        List[int]: 包含所有众数的列表。
+    判断一棵二叉树是否是高度平衡的。高度平衡的二叉树是指任何一个节点的两个子树的高度差不超过1。
+    
+    参数：
+        root (Optional[TreeNode]): 二叉树的根节点。
+    
+    返回：
+        bool: 如果二叉树是高度平衡的，则返回True；否则返回False。
     """
-    result = []
-    maxFreq = 0
-    cur = None
-    freq = 0
+    def height(root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return max(height(root.left), height(root.right)) + 1
+    
+    if not root:
+        return True
 
-    def dfs(node):
-        nonlocal cur, maxFreq, result, freq
-        if not node: return
-        dfs(node.left)
-        if cur == node.val:
-            freq += 1
-        else:
-            freq = 1
-            cur = node.val
-        if freq == maxFreq:
-            result.append(cur)
-        elif freq > maxFreq:
-            result = [cur]
-            maxFreq = freq
-        dfs(node.right)
-    dfs(root)
-    return result
+    if abs(height(root.left) - height(root.right)) > 1:
+        return False
+        
+    return isBalanced(root.left) and isBalanced(root.right)
 
 
-def getMinimumDifference(root: Optional[TreeNode]) -> int:
+def isSubtree(root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
     """
-    计算给定二叉搜索树中任意两个节点值的最小差异。
-
-    参数:
-        root (TreeNode, 可选): 二叉搜索树的根节点。
-
-    返回:
-        int: 最小差异的数值。
-    """
-    stack = []
-    result = float('inf')
-    previous = None
-    while root or stack:
-        if root:
-            stack.append(root)
-            root = root.left
-        else:
-            current = stack.pop()
-            if previous:  result = min(current.val - previous.val, result)
-            previous = current
-            root = current.right
-    return result
-
-
-def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
-    """
-    计算二叉树的直径，即二叉树中任意两个节点之间的最长路径的边数。
+    判断一个二叉树是否是另一个二叉树的子树。
     
     参数:
-        root (TreeNode, 可选): 二叉树的根节点。
+        root (TreeNode, 可选): 主树的根节点。
+        subRoot (TreeNode, 可选): 子树的根节点。
 
     返回:
-        int: 二叉树的直径。
+        bool: 如果 subRoot 是 root 的子树，返回 True，否则返回 False。
     """
-    ans = 0
-    def dfs(node: Optional[TreeNode]) -> int:
-        if node is None:
-            return -1
-        l_len = dfs(node.left) + 1
-        r_len = dfs(node.right) + 1
-        nonlocal ans
-        ans = max(ans, l_len + r_len)
-        return max(l_len, r_len)
-    dfs(root)
-    return ans
-
-
-def findTilt(root: Optional[TreeNode]) -> int:
-    """
-    计算二叉树的所有节点的倾斜度总和。节点的倾斜度定义为该节点的左子树节点值之和与右子树节点值之和的绝对差。
+    def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p or not q:  return p is q
+        if p.val != q.val:  return False
+        return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
     
+    if not root or not subRoot:
+        return root is subRoot
+    return isSameTree(root, subRoot) or isSubtree(root.left, subRoot) or isSubtree(root.right, subRoot)
+
+
+def mergeTrees(root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+    """
+    合并两棵二叉树，每个对应节点的值相加，如果一个位置只有一个树有节点，则直接使用该节点。
+
     参数:
-        root (TreeNode, 可选): 二叉树的根节点。
-    
+        root1 (TreeNode, 可选): 第一棵二叉树的根节点。
+        root2 (TreeNode, 可选): 第二棵二叉树的根节点。
+
     返回:
-        int: 所有节点的倾斜度之和。
+        TreeNode, 可选: 合并后的二叉树的根节点，如果两棵树均为空，则返回 None。
     """
-    ans = 0
-    def dfs(root: Optional[TreeNode]) -> int:
-        if not root:  return 0
-        sum_left = dfs(root.left)
-        sum_right = dfs(root.right)
-        nonlocal ans
-        ans += abs(sum_left - sum_right)
-        return sum_left + sum_right + root.val
-    dfs(root)
-    return ans
+    if not root1:
+        return root2
+    
+    if not root2:
+        return root1
+    
+    return TreeNode(root1.val + root2.val,
+        mergeTrees(root1.left, root2.left),
+        mergeTrees(root1.right, root2.right))
