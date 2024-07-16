8d3fb47cd855e30af80fe277937bd5bc5335c31b
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-16 13:39:53 +0800

    Update Template_01_BinaryTree.py

--- Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
+++ Tree-Based Algorithms Catalog/Template_01_BinaryTree.py
@@ -121,109 +121,14 @@ def isBalanced(root: Optional[TreeNode]) -> bool:
     
     if not root:
         return True
-    
     if abs(height(root.left) - height(root.right)) > 1:
         return False
-        
     return isBalanced(root.left) and isBalanced(root.right)
-
-
-def searchBST(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-    """
-    在二叉搜索树中搜索给定值的节点。如果存在这样的节点，则返回该节点；否则返回 None。
-
-    参数:
-        root (TreeNode, 可选): 二叉搜索树的根节点。
-        val (int): 需要搜索的节点值。
-
-    返回:
-        TreeNode, 可选: 包含给定值的节点，如果没有找到则返回 None。
-    """
-    if not root:
-        return None
-        
-    if root.val == val:
-        return root
-        
-    return searchBST(root.left if val < root.val else root.right, val)
-
-
-def minDepth(root: Optional[TreeNode]) -> int:
-    """
-    计算二叉树的最小深度。最小深度是从根节点到最近叶子节点的最短路径上的节点数。
-    
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-    
-    返回：
-        int: 二叉树的最小深度。
-    """
-    if root is None:
-        return 0
-        
-    if root.right is None:
-        return minDepth(root.left) + 1
-        
-    if root.left is None:
-        return minDepth(root.right) + 1
-        
-    return min(minDepth(root.left), minDepth(root.right)) + 1
-
-
-def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
-    """
-    判断二叉树中是否存在一条路径，该路径上的节点值之和等于给定的目标值 targetSum。
-    
-    参数：
-        root (Optional[TreeNode]): 二叉树的根节点。
-        targetSum (int): 路径和的目标值。
-    
-    返回：
-        bool: 如果存在这样的路径，则返回 True；否则返回 False。
-    """
-    def hasPathSum(root: Optional[TreeNode], targetSum: int) -> bool:
-        isLeafNode = lambda node: not node.left and not node.right
-        
-        if not root:  return False
-            
-        if isLeafNode(root):
-            return targetSum == root.val
-            
-        return hasPathSum(root.left, targetSum - root.val) or hasPathSum(root.right, targetSum - root.val)
-
-
-def sortedArrayToBST(nums: List[int]) -> Optional[TreeNode]:
-    """
-    将一个按升序排列的整数数组转换为一棵高度平衡的二叉搜索树。高度平衡的二叉树是指每个节点的两个子树的深度差不超过1。
-    
-    参数：
-        nums (List[int]): 升序排列的整数数组。
-    
-    返回：
-        Optional[TreeNode]: 高度平衡二叉搜索树的根节点。
-    """
-    def helper(left: int, right: int) -> Optional[TreeNode]:
-        if left > right:
-            return None
-        
-        mid = (left + right) // 2
-        root = TreeNode(nums[mid])
-        root.left = helper(left, mid - 1)
-        root.right = helper(mid + 1, right)
-        return root
-    
-    return helper(0, len(nums) - 1)
 
 
 def sumOfLeftLeaves(root: Optional[TreeNode]) -> int:
     """
     计算给定二叉树中所有左叶子节点的值之和。
-
-    参数:
-        root (Optional[TreeNode]): 二叉树的根节点。
-
-    返回:
-        int: 所有左叶子节点的值之和。
     """
     isLeafNode = lambda node: not node.left and not node.right
     def dfs(node: Optional[TreeNode]) -> int:
@@ -234,31 +139,6 @@ def sumOfLeftLeaves(root: Optional[TreeNode]) -> int:
             ans += dfs(node.right)
         return ans
     return dfs(root) if root else 0
-
-
-def getMinimumDifference(root: Optional[TreeNode]) -> int:
-    """
-    计算给定二叉搜索树中任意两个节点值的最小差异。
-
-    参数:
-        root (TreeNode, 可选): 二叉搜索树的根节点。
-
-    返回:
-        int: 最小差异的数值。
-    """
-    stack = []
-    result = float('inf')
-    previous = None
-    while root or stack:
-        if root:
-            stack.append(root)
-            root = root.left
-        else:
-            current = stack.pop()
-            if previous:  result = min(current.val - previous.val, result)
-            previous = current
-            root = current.right
-    return result
 
 
 def diameterOfBinaryTree(root: Optional[TreeNode]) -> int:
