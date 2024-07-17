3cf5d946721dd70e4acc932dd7b16cf712dc32ab
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-17 20:05:08 +0800

    Update Template_SingleLinkedList.py

--- Template_SingleLinkedList.py
+++ Template_SingleLinkedList.py
@@ -8,13 +8,6 @@
 def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     """
     合并两个已排序的链表，并返回一个新的排序链表。新链表是通过将两个链表的节点依次连接而成的。
-    
-    参数：
-        list1 (Optional[ListNode]): 第一个排序链表。
-        list2 (Optional[ListNode]): 第二个排序链表。
-    
-    返回：
-        Optional[ListNode]: 合并后的排序链表的头节点。
     """
     dummy = cur = ListNode()    
     while list1 and list2:
@@ -33,12 +26,6 @@ def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optio
 def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
     """
     删除排序链表中的所有重复元素，使每个元素只出现一次，并返回修改后的链表的头节点。
-
-    参数：
-        head (Optional[ListNode]): 排序链表的头节点。
-    
-    返回：
-        Optional[ListNode]: 删除重复元素后的链表的头节点。
     """
     if not head:
         return None
@@ -54,12 +41,6 @@ def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
 def hasCycle(head: Optional[ListNode]) -> bool:
     """
     判断链表中是否有环。使用快慢指针方法，快指针每次移动两步，慢指针每次移动一步，如果链表有环，则快慢指针最终会相遇。
-
-    参数：
-        head (Optional[ListNode]): 链表的头节点。
-    
-    返回：
-        bool: 如果链表中有环，则返回True；否则返回False。
     """
     slow = head
     fast = head
@@ -75,13 +56,6 @@ def getIntersectionNode(headA: ListNode, headB: ListNode) -> Optional[ListNode]:
     """
     寻找两个单链表相交的起始节点。如果两个链表没有交点，返回 None。
     使用两个指针分别遍历两个链表，当达到链表末尾时，转向另一个链表的头部继续遍历，直到两个指针相遇。
-    
-    参数：
-        headA (ListNode): 第一个链表的头节点。
-        headB (ListNode): 第二个链表的头节点。
-    
-    返回：
-        Optional[ListNode]: 两个链表相交的节点，如果不相交，则返回 None。
     """
     A, B = headA, headB
     while A != B:
@@ -94,13 +68,6 @@ def removeElements(head: Optional[ListNode], val: int) -> Optional[ListNode]:
     """
     从链表中删除所有值等于指定值 val 的节点。
     使用虚拟头节点简化边界条件处理，确保即使头节点需要被删除时也能正确处理。
-
-    参数：
-        head (Optional[ListNode]): 链表的头节点。
-        val (int): 需要删除的节点值。
-    
-    返回：
-        Optional[ListNode]: 删除指定值节点后的链表头节点。
     """
     dummy = cur = ListNode(next=head)
     while cur.next:
@@ -114,12 +81,6 @@ def removeElements(head: Optional[ListNode], val: int) -> Optional[ListNode]:
 def isPalindrome(head: Optional[ListNode]) -> bool:
     """
     判断给定的链表是否为回文链表。使用快慢指针找到链表中点，然后翻转后半部分，之后比较前半部分和翻转后的后半部分是否相同。
-
-    参数:
-        head (Optional[ListNode]): 链表的头节点。
-
-    返回:
-        bool: 如果链表是回文结构则返回 True，否则返回 False。
     """
     def middleList(head: Optional[ListNode]) -> Optional[ListNode]:
         fast = slow = head
@@ -136,16 +97,13 @@ def isPalindrome(head: Optional[ListNode]) -> bool:
             current.next = previous
             previous = current
             current = cur_next
-        
         return previous
         
     first = head
     second = reverseList(middleList(head))
-
     while second:
         if second.val != first.val:
             return False
         second = second.next
         first = first.next
-    
     return True
