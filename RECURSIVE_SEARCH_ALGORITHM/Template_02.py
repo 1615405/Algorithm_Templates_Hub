bb9b253e645c3b65ffdfcbe95da954991386dbbe
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-19 20:16:28 +0800

    Update Template_02.py

--- RECURSIVE_SEARCH_ALGORITHM/Template_02.py
+++ RECURSIVE_SEARCH_ALGORITHM/Template_02.py
@@ -3,17 +3,18 @@ def isOneBitCharacter(bits: List[int]) -> bool:
     判断由 1-bit 和 2-bit 组成的数组中最后一个字符是否为一个 1-bit 字符。
     """
     n = len(bits)
-    
+
     @cache
     def can_decode(start: int) -> bool:
         if start == n - 1:
             return bits[start] == 0
-        if start >= n:
+        if start == n:
             return False
         if bits[start] == 0:
             return can_decode(start + 1)
-        if start + 1 < n and bits[start] == 1:
+        if start < n - 1 and bits[start] == 1:
             return can_decode(start + 2)
+    
     return can_decode(0)
 
 
@@ -27,4 +28,5 @@ def minCostClimbingStairs(cost: List[int]) -> int:
         if i <= 1:
             return 0
         return min(dfs(i - 2) + cost[i - 2], dfs(i - 1) + cost[i - 1])
+    
     return dfs(len(cost))
