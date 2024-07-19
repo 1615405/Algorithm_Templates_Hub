9b9d3c5226fcba52917d9f30e69c02cb02a4164f
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-19 20:29:46 +0800

    Update Template_03.py

--- RECURSIVE_SEARCH_ALGORITHM/Template_03.py
+++ RECURSIVE_SEARCH_ALGORITHM/Template_03.py
@@ -1,3 +1,24 @@
+def floodFill(image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+    """
+    对图像执行泛洪填充操作。
+    """
+    curr_color = image[sr][sc]
+    if curr_color == color:
+        return image
+    
+    n, m = len(image), len(image[0])
+    q = deque([(sr, sc)])
+    image[sr][sc] = color
+    while q:
+        x, y = q.popleft()
+        for nx, ny in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+            if 0 <= nx < n and 0 <= ny < m and image[nx][ny] == curr_color:
+                image[nx][ny] = color
+                q.append((nx, ny))
+                
+    return image
+    
+
 def islandPerimeter(grid: List[List[int]]) -> int:
     """
     计算在给定的二维网格中, 由1组成的岛屿的周长。
@@ -23,24 +44,3 @@ def islandPerimeter(grid: List[List[int]]) -> int:
         for j in range(n):
             result += dfs(i, j) if grid[i][j] == 1 else 0
     return result
-
-
-def floodFill(image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-    """
-    对图像执行“泛洪填充”操作。
-    """
-    currColor = image[sr][sc]
-    if currColor == color:
-        return image
-    
-    n, m = len(image), len(image[0])
-    
-    que = collections.deque([(sr, sc)])
-    image[sr][sc] = color
-    while que:
-        x, y = que.popleft()
-        for mx, my in [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]:
-            if 0 <= mx < n and 0 <= my < m and image[mx][my] == currColor:
-                image[mx][my] = color
-                que.append((mx, my))
-    return image
