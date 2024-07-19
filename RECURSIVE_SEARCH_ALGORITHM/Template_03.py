c5c229a9075dbc1efcf33868bc0213529a97000d
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-19 22:12:03 +0800

    Update Template_03.py

--- RECURSIVE_SEARCH_ALGORITHM/Template_03.py
+++ RECURSIVE_SEARCH_ALGORITHM/Template_03.py
@@ -41,3 +41,27 @@ def islandPerimeter(grid: List[List[int]]) -> int:
         for j in range(m):
             perimeter += dfs(i, j) if grid[i][j] == 1 else 0
     return perimeter
+
+
+def maxAreaOfIsland(grid: List[List[int]]) -> int:
+    """
+    计算二维网格中最大的岛屿面积。岛屿由相邻的陆地组成，陆地由“1”表示，水域由“0”表示。岛屿可以在水平或垂直方向相邻，但不能对角相连。
+    """
+    n, m = len(grid), len(grid[0])
+    
+    def dfs(x: int, y: int) -> int:
+        if x < 0 or x >= n or y < 0 or y >= m or grid[x][y] == 0:
+            return 0
+        grid[x][y] = 0
+        ans = 1
+        for dx, dy in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+            ans += dfs(dx, dy)
+        return ans
+    
+    max_area = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 1:
+                max_area = max(max_area, dfs(i, j))
+    
+    return max_area
