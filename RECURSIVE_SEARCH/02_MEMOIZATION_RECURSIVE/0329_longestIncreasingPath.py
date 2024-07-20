7eeb853348c1e8b4b0ecba39f8449ed8324c36b4
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-20 12:53:57 +0800

    Create 0329_longestIncreasingPath.py

---
+++ RECURSIVE_SEARCH/02_MEMOIZATION_RECURSIVE/0329_longestIncreasingPath.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+
+        @cache
+        def dfs(x: int, y: int) -> int:
+            longest_path = 1
+            for dx, dy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
+                if 0 <= dx < n and 0 <= dy < m and matrix[dx][dy] < matrix[x][y]:
+                    longest_path = max(longest_path, dfs(dx, dy) + 1)
+            return longest_path
+        
+        return max(dfs(i, j) for j in range(m) for i in range(n))
