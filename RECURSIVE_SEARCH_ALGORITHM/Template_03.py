93f14c472cf01ee845ff4618aa5696b241e67b36
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-20 11:56:35 +0800

    Update Template_03.py

--- RECURSIVE_SEARCH_ALGORITHM/Template_03.py
+++ RECURSIVE_SEARCH_ALGORITHM/Template_03.py
@@ -150,3 +150,29 @@ def solve(board: List[List[str]]) -> None:
                 board[i][j] = "O"
             elif board[i][j] == "O":
                 board[i][j] = "X"
+
+
+def numEnclaves(grid: List[List[int]]) -> int:
+    """
+    计算二维网格中完全被水包围的陆地单元格的数量。陆地单元格定义为值为 1 的单元格，
+    如果一个陆地单元格无法通过上下左右移动到达边界，则认为这个陆地单元格是被完全包围的。
+    """
+    n, m = len(grid), len(grid[0])
+    visited = [[False] * m for _ in range(n)]
+
+    def dfs(x: int, y: int) -> None:
+        if x < 0 or x >= n or y < 0 or y >= m or grid[x][y] == 0 or visited[x][y]:
+            return
+        visited[x][y] = True
+        for dx, dy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
+            dfs(dx, dy)
+    
+    for i in range(n):
+        dfs(i, 0)
+        dfs(i, m - 1)
+    
+    for j in range(m - 1):
+        dfs(0, j)
+        dfs(n - 1, j)
+    
+    return sum(grid[i][j] == 1 and not visited[i][j] for j in range(1, m - 1) for i in range(1, n - 1))
