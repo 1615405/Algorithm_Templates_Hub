98af3bf2498050a0b9abbef7b81fab378a9ee30d
cherry_77 <125164331+1615405@users.noreply.github.com>
2024-07-23 12:34:35 +0800

    Update LC_0739.py

--- 单调栈/LC_0739.py
+++ 单调栈/LC_0739.py
@@ -12,3 +12,15 @@ class Solution:
             rank[idx] = idx
         
         return [rank[i] - i for i, _ in enumerate(temperatures)]
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        rank = defaultdict(int)
+        for i in range(len(temperatures) - 1, -1, -1):
+            while stack and temperatures[stack[-1]] <= temperatures[i]:
+                stack.pop()
+            rank[i] = stack[-1] if stack else i
+            stack.append(i)
+        return [rank[i] - i for i in range(len(temperatures))]
